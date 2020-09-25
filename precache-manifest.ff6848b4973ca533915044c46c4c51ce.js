dc30d66ddd799d72008afa6ef02dd2c7f5e7d355
Fatih Kurt <fatihkurt@Fatih-MacBook-Air.local>
2020-09-25 14:21:13 +0300

    Updates

---
+++ precache-manifest.ff6848b4973ca533915044c46c4c51ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0af3b5cff49201461529be3171e3c03f",
+    "url": "/flex-pomodoro-app/index.html"
+  },
+  {
+    "revision": "4d43711f4c12630de9a6",
+    "url": "/flex-pomodoro-app/static/css/2.8ce86be7.chunk.css"
+  },
+  {
+    "revision": "23fbe71a16c225a4970e",
+    "url": "/flex-pomodoro-app/static/css/main.2055938a.chunk.css"
+  },
+  {
+    "revision": "4d43711f4c12630de9a6",
+    "url": "/flex-pomodoro-app/static/js/2.66973b2e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/flex-pomodoro-app/static/js/2.66973b2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23fbe71a16c225a4970e",
+    "url": "/flex-pomodoro-app/static/js/main.1f39a994.chunk.js"
+  },
+  {
+    "revision": "4694fb40e44e288caf74",
+    "url": "/flex-pomodoro-app/static/js/runtime-main.f60b1873.js"
+  }
+]);
